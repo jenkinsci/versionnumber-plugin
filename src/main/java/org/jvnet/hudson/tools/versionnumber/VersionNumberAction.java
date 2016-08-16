e8512cedd2b1ae9cf7b1e435c9e612f90a00a123
Michael Fowler <michaeldkfowler@gmail.com>
2016-08-16 15:45:40 -0400

    [FIXED JENKINS-37451] Exposes versionNumber to the API Cleaned up warnings

--- src/main/java/org/jvnet/hudson/tools/versionnumber/VersionNumberAction.java
+++ src/main/java/org/jvnet/hudson/tools/versionnumber/VersionNumberAction.java
@@ -1,12 +1,15 @@
 package org.jvnet.hudson.tools.versionnumber;
 
 import hudson.model.Action;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
 
+@ExportedBean(defaultVisibility=999)
 public class VersionNumberAction implements Action {
     static final String ICON = "/plugin/versionnumber/vnicon_24x24.gif";
     
-    private VersionNumberBuildInfo info;
-    private String versionNumber;
+    private final VersionNumberBuildInfo info;
+    private final String versionNumber;
     
     public VersionNumberAction(VersionNumberBuildInfo info, String versionNumber) {
         this.info = info;
@@ -17,18 +20,23 @@ public class VersionNumberAction implements Action {
         return info;
     }
 
+    @Exported
     public String getVersionNumber() {
 	return this.versionNumber;
     }
 
+
+    @Override
     public String getDisplayName() {
         return "Version " + this.versionNumber;
     }
 
+    @Override
     public String getIconFileName() {
         return ICON;
     }
     
+    @Override
     public String getUrlName() {
         return "versionnumber/displayName";
     }
