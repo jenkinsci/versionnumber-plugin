a74ed01cac6186fabc8c232d8dac46a24db284d0
strangelookingnerd <strangelookingnerd@users.noreply.github.com>
2025-02-17 11:52:25 +0100

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor clean up

--- src/test/java/org/jvnet/hudson/tools/versionnumber/VersionNumberBuilderTest.java
+++ src/test/java/org/jvnet/hudson/tools/versionnumber/VersionNumberBuilderTest.java
@@ -1,158 +1,193 @@
 package org.jvnet.hudson.tools.versionnumber;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.Launcher;
+import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.model.FreeStyleBuild;
-import hudson.model.Result;
-import hudson.model.AbstractBuild;
 import hudson.model.FreeStyleProject;
+import hudson.model.Result;
 import hudson.scm.NullSCM;
 import hudson.slaves.EnvironmentVariablesNodeProperty;
-
 import java.io.File;
-import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+@WithJenkins
+class VersionNumberBuilderTest {
 
-public class VersionNumberBuilderTest extends HudsonTestCase {
-
-    public void testIncrementBuildsAllTimeByVersionNumberPrefix() throws Exception {
-        
-        FreeStyleProject job = super.createFreeStyleProject("versionNumberJob");
+    @Test
+    void testIncrementBuildsAllTimeByVersionNumberPrefix(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         FreeStyleBuild build;
-        
+
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "1.0.${BUILDS_ALL_TIME}", null, null, "VERSION_PREFIX", null, null, null, null, null, false);
-        
+
         VersionNumberBuilder versionNumberBuilderWithPrefix = new VersionNumberBuilder(
-                "${VERSION_PREFIX}${BUILDS_ALL_TIME}", null, null, "VERSION_PREFIX", null, null, null, null, null, false);
-        
+                "${VERSION_PREFIX}${BUILDS_ALL_TIME}",
+                null,
+                null,
+                "VERSION_PREFIX",
+                null,
+                null,
+                null,
+                null,
+                null,
+                false);
+
         job.getBuildWrappersList().add(versionNumberBuilder);
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(1, build);
-        
-        build = super.buildAndAssertSuccess(job);  // The pull request #2 (by @mranostay) causes a null pointer here.
-        this.assertBuildsAllTime(2, build);
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(1, build);
+
+        build = r.buildAndAssertSuccess(job); // The pull request #2 (by @mranostay) causes a null pointer here.
+        assertBuildsAllTime(2, build);
 
         job.getBuildWrappersList().clear();
         job.getBuildWrappersList().add(versionNumberBuilderWithPrefix);
-        
+
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars envVars = prop.getEnvVars();
-        super.hudson.getGlobalNodeProperties().add(prop);
-        
+        r.jenkins.getGlobalNodeProperties().add(prop);
+
         envVars.put("VERSION_PREFIX", "2.0.");
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(1, build);
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(2, build);
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(3, build);
-        
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(1, build);
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(2, build);
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(3, build);
+
         envVars.put("VERSION_PREFIX", "2.5.");
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(1, build);
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(2, build);
-        
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(1, build);
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(2, build);
+
         envVars.put("VERSION_PREFIX", "2.0.");
-        
-        build = super.buildAndAssertSuccess(job);
-        this.assertBuildsAllTime(4, build);
+
+        build = r.buildAndAssertSuccess(job);
+        assertBuildsAllTime(4, build);
     }
-    
-    public void testTwoBuilds() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+
+    @Test
+    void testTwoBuilds(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "1.0.${BUILDS_ALL_TIME}", null, null, null, null, null, null, null, null, false);
         job.getBuildWrappersList().add(versionNumberBuilder);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
-        build = buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
+        build = r.buildAndAssertSuccess(job);
         assertBuildsAllTime(2, build);
     }
-    
+
     // see #HUDSON-7933
-    public void testFailureEarlyDoesNotResetVersionNumber() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+    @Test
+    void testFailureEarlyDoesNotResetVersionNumber(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "1.0.${BUILDS_ALL_TIME}", null, null, null, null, null, null, null, null, false);
         job.getBuildWrappersList().add(versionNumberBuilder);
-        buildAndAssertSuccess(job);
-        buildAndAssertSuccess(job);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
+        r.buildAndAssertSuccess(job);
+        r.buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
         assertBuildsAllTime(3, build);
         job.setScm(new FailureSCM());
         build = job.scheduleBuild2(0).get();
-        assertBuildStatus(Result.FAILURE, build);
+        r.assertBuildStatus(Result.FAILURE, build);
         // When build fails very early, there will be no VersionNumber attached
-        // to it. 
+        // to it.
         assertNull(build.getAction(VersionNumberAction.class));
         job.setScm(new NullSCM());
-        build = buildAndAssertSuccess(job);
+        build = r.buildAndAssertSuccess(job);
         assertBuildsAllTime(4, build);
     }
-    
-    public void testUseAsBuildDisplayName() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+
+    @Test
+    void testUseAsBuildDisplayName(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "1.0.${BUILDS_ALL_TIME}", null, null, null, null, null, null, null, null, false, true);
         job.getBuildWrappersList().add(versionNumberBuilder);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
         assertEquals("1.0.1", build.getDisplayName());
-        build = buildAndAssertSuccess(job);
+        build = r.buildAndAssertSuccess(job);
         assertEquals("1.0.2", build.getDisplayName());
     }
-    
-    public void testValueFromEnvironmentVariable() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+
+    @Test
+    void testValueFromEnvironmentVariable(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "${BUILDS_TODAY}.${BUILDS_THIS_WEEK}.${BUILDS_THIS_MONTH}.${BUILDS_THIS_YEAR}.${BUILDS_ALL_TIME}",
-                null, null, null, "${ENVVAL_OF_TODAY}", "${ENVVAL_OF_THIS_WEEK}", "${ENVVAL_OF_THIS_MONTH}", "${ENVVAL_OF_THIS_YEAR}", "${ENVVAL_OF_ALL_TIME}", false, true);
-        
+                null,
+                null,
+                null,
+                "${ENVVAL_OF_TODAY}",
+                "${ENVVAL_OF_THIS_WEEK}",
+                "${ENVVAL_OF_THIS_MONTH}",
+                "${ENVVAL_OF_THIS_YEAR}",
+                "${ENVVAL_OF_ALL_TIME}",
+                false,
+                true);
+
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars envVars = prop.getEnvVars();
-        envVars.put("ENVVAL_OF_TODAY", "-10");           // Invalid (negative) value
-        envVars.put("ENVVAL_OF_THIS_WEEK", "2.0");       // Invalid (float number) value
-        envVars.put("ENVVAL_OF_THIS_MONTH", "Invalid");  // Invalid (non-number) value
-        //envVars.put("ENVVAL_OF_THIS_YEAR", "");        // No variable
-        envVars.put("ENVVAL_OF_ALL_TIME", "20");         // Normal value
-        super.hudson.getGlobalNodeProperties().add(prop);
-        
+        envVars.put("ENVVAL_OF_TODAY", "-10"); // Invalid (negative) value
+        envVars.put("ENVVAL_OF_THIS_WEEK", "2.0"); // Invalid (float number) value
+        envVars.put("ENVVAL_OF_THIS_MONTH", "Invalid"); // Invalid (non-number) value
+        // envVars.put("ENVVAL_OF_THIS_YEAR", "");        // No variable
+        envVars.put("ENVVAL_OF_ALL_TIME", "20"); // Normal value
+        r.jenkins.getGlobalNodeProperties().add(prop);
+
         job.getBuildWrappersList().add(versionNumberBuilder);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
         assertEquals("1.1.1.1.20", build.getDisplayName());
-        
+
         // make sure that the overrides via environment variables remain
         assertEquals("${ENVVAL_OF_ALL_TIME}", versionNumberBuilder.getBuildsAllTime());
         assertEquals("${ENVVAL_OF_TODAY}", versionNumberBuilder.getBuildsToday());
         assertEquals("${ENVVAL_OF_THIS_WEEK}", versionNumberBuilder.getBuildsThisWeek());
         assertEquals("${ENVVAL_OF_THIS_MONTH}", versionNumberBuilder.getBuildsThisMonth());
         assertEquals("${ENVVAL_OF_THIS_YEAR}", versionNumberBuilder.getBuildsThisYear());
-        
     }
-    
-    public void testThatOverrideValuesGetResetAfterJobRuns() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+
+    @Test
+    void testThatOverrideValuesGetResetAfterJobRuns(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
                 "${BUILDS_TODAY}.${BUILDS_THIS_WEEK}.${BUILDS_THIS_MONTH}.${BUILDS_THIS_YEAR}.${BUILDS_ALL_TIME}",
-                null, null, null, "1", "7", "30", "365", "500", false, true);
-        
+                null,
+                null,
+                null,
+                "1",
+                "7",
+                "30",
+                "365",
+                "500",
+                false,
+                true);
+
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars envVars = prop.getEnvVars();
-        super.hudson.getGlobalNodeProperties().add(prop);
-        
+        r.jenkins.getGlobalNodeProperties().add(prop);
+
         job.getBuildWrappersList().add(versionNumberBuilder);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
         assertEquals("1.7.30.365.500", build.getDisplayName());
-        
+
         // make sure that the direct set variables were cleaned up
         assertEquals("", versionNumberBuilder.getBuildsAllTime());
         assertEquals("", versionNumberBuilder.getBuildsToday());
@@ -160,38 +195,50 @@ public class VersionNumberBuilderTest extends HudsonTestCase {
         assertEquals("", versionNumberBuilder.getBuildsThisMonth());
         assertEquals("", versionNumberBuilder.getBuildsThisYear());
     }
-    
-    public void testSubstringFromEnvironmentVariable() throws Exception {
-        FreeStyleProject job = createFreeStyleProject("versionNumberJob");
+
+    @Test
+    void testSubstringFromEnvironmentVariable(JenkinsRule r) throws Exception {
+        FreeStyleProject job = r.createFreeStyleProject("versionNumberJob");
         VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
-                "${ENVVAL_TESTING, \"+3\"}.${ENVVAL_TESTING, \"2\"}.${ENVVAL_TESTING, \"-3\"}" +
-                ".${ENVVAL_TESTING, \"5\"}.${ENVVAL_TESTING, \"+5\"}.${ENVVAL_TESTING, \"-5\"}" +
-                ".${ENVVAL_TESTING, \"0\"}.${ENVVAL_TESTING, \"1.5\"}.${ENVVAL_TESTING, \"test\"}",
-                null, "${ENVVAL_TESTING}", null, null, null, null, null, null, false, true);
+                "${ENVVAL_TESTING, \"+3\"}.${ENVVAL_TESTING, \"2\"}.${ENVVAL_TESTING, \"-3\"}"
+                        + ".${ENVVAL_TESTING, \"5\"}.${ENVVAL_TESTING, \"+5\"}.${ENVVAL_TESTING, \"-5\"}"
+                        + ".${ENVVAL_TESTING, \"0\"}.${ENVVAL_TESTING, \"1.5\"}.${ENVVAL_TESTING, \"test\"}",
+                null,
+                "${ENVVAL_TESTING}",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                false,
+                true);
         // tried
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars envVars = prop.getEnvVars();
         envVars.put("ENVVAL_TESTING", "1234");
-        super.hudson.getGlobalNodeProperties().add(prop);
-        
+        r.jenkins.getGlobalNodeProperties().add(prop);
+
         job.getBuildWrappersList().add(versionNumberBuilder);
-        FreeStyleBuild build = buildAndAssertSuccess(job);
+        FreeStyleBuild build = r.buildAndAssertSuccess(job);
         assertEquals("123.12.234.1234.1234.1234.1234.1234.1234", build.getDisplayName());
     }
-    
-    private void assertBuildsAllTime(int expected, AbstractBuild build) {
-        VersionNumberAction versionNumberAction = build
-                .getAction(VersionNumberAction.class);
+
+    private static void assertBuildsAllTime(int expected, AbstractBuild build) {
+        VersionNumberAction versionNumberAction = build.getAction(VersionNumberAction.class);
         assertEquals(expected, versionNumberAction.getInfo().getBuildsAllTime());
     }
-    
+
     private static class FailureSCM extends NullSCM {
         @Override
-        public boolean checkout(AbstractBuild<?, ?> build, Launcher launcher,
-                FilePath remoteDir, BuildListener listener, File changeLogFile)
-                throws IOException, InterruptedException {
+        @Deprecated
+        public boolean checkout(
+                AbstractBuild<?, ?> build,
+                Launcher launcher,
+                FilePath remoteDir,
+                BuildListener listener,
+                @NonNull File changeLogFile) {
             return false;
         }
     }
-    
 }
